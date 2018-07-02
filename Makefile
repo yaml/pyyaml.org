2e2f864dc629d781ac8ec04d4a53b7a28a9533a6
Tina Müller <cpan2@tinita.de>
2018-07-02 19:50:12 +0200

    Add index.html files for download directories

---
+++ Makefile
@@ -0,0 +1,2 @@
+update:
+	./bin/update-index-html.pl download download/libyaml download/pysyck download/pyyaml download/pyyaml-legacy
---
+++ bin/update-index-html.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+use Data::Dumper;
+
+my (@dirs) = @ARGV;
+
+for my $dir (@dirs) {
+    update($dir);
+}
+
+sub update {
+    my ($dir) = @_;
+    # remove trailing slash if there is one
+    $dir =~ s{/$}{};
+
+    opendir my $dh, $dir or die $!;
+    my @files = grep {
+        not m/^\./ and $_ ne 'index.html'
+    } readdir $dh;
+    closedir $dh;
+
+    my $listing = join "\n", map {
+        my $size = -s "$dir/$_";
+        qq{<tr><td><tt><a href="$_">$_</a></tt></td><td><tt>$size</tt></td></tr>}
+    } sort @files;
+
+    my $html = <<"EOM";
+<html>
+<head><title>Index of /$dir/</title></head>
+<body bgcolor="white">
+<h1>Index of /$dir/</h1><hr>
+<table>
+<tr><td><tt><a href="../">../</a></td><td></td></tr>
+$listing
+</table>
+<hr></body>
+</html>
+EOM
+
+    open my $fh, '>', "$dir/index.html" or die $!;
+    print $fh $html;
+    close $fh;
+}
